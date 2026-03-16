91d80bb4a6d8a4ce6c0ca071f9cf3730f5a1b1b4
Youssef Eddaif <183735203+j0d3v@users.noreply.github.com>
2026-03-16 07:18:53 +0000

    Some minor changes
    Signed-off-by: Youssef Eddaif <183735203+j0d3v@users.noreply.github.com>

--- sway/autostart.sh
+++ sway/autostart.sh
@@ -2,9 +2,8 @@
 dbus-update-activation-environment --all  &
 dunst &
 nm-applet &
-lxqt-policykit-agent &
+/usr/libexec/polkit-mate-authentication-agent-1 &
 pipewire &
 pipewire-pulse &
 /usr/libexec/xdg-desktop-portal-gtk &
 wl-paste -t text --watch clipman store --no-persist &
-brightnessctl s 1020
